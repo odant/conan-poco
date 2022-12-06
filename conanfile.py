4e579752ad57d4583128b20b6bad822c069fa595
d0nc1h0t <udincev@yandex.ru>
2022-12-06 12:16:28 +0300

    Version 1.12.4+0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os
 
 class PocoConan(ConanFile):
     name = "poco"
-    version = "1.11.3+0"
+    version = "1.12.4+0"
     license = "Boost Software License Version 1.0. https://pocoproject.org/license.html"
     description = "Modern, powerful open source C++ class libraries for building network- and internet-based applications that run on desktop, server, mobile and embedded systems."
     url = "https://github.com/odant/conan-poco"
